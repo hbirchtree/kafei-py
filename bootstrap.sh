ec45331b6edd22556f2df577d22a0189d59499a2
Håvard Bjerke <h0wy36@gmail.com>
2018-03-05 22:21:57 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Update repositories, get some packages
-apt update
-apt -y install apt-transport-https ca-certificates curl software-properties-common python3-pip
+apt-get -qy update
+apt-get -qy install apt-transport-https ca-certificates curl software-properties-common python3-pip
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-apt update
-apt -y install docker.io
-apt -y install docker-compose
+apt-get -q update
+apt-get -qy install docker.io
+apt-get -qy install docker-compose
 
 # Create Kafei contained user
 adduser \
