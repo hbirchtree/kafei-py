7f5a174c945f0b01b3520b1023b7c9f77784d2b9
Håvard Bjerke <h0wy36@gmail.com>
2018-03-05 22:53:20 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Do this to avoid locale errors
 unset LC_MESSAGES
 unset LC_COLLATE
 unset LC_CTYPE
@@ -8,16 +9,17 @@ unset LC_CTYPE
 apt-get -qy update
 apt-get -qy install apt-transport-https ca-certificates curl software-properties-common python3-pip tmux
 
+# Add Docker and Certbot repositories
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 add-apt-repository ppa:certbot/certbot
 
+# Install said packages
 apt-get -q update
 apt-get -qy install docker.io certbot
 
 # Create Kafei contained user
-adduser \
+adduser kafei \
     --home /home/kafei \
     --uid 1000 \
     --disabled-password \
@@ -26,6 +28,12 @@ adduser \
 # Add user to `docker` group
 usermod -a -G docker kafei
 
+# Clone repository and start containers
 su -c 'if [ ! $(grep .local ~/.bashrc) ]; then echo \'PATH=$PATH:~/.local/bin\' >> ~/.bashrc; fi'
 su -c 'cd ~ && git clone https://github.com/hbirchtree/kafei-py.git && cd kafei-py && pip3 install -r requirements.txt' kafei
+
+# Get certificates
+bash /home/kafei/kafei-py/get-certs.sh
+
+# Start servers
 su -c 'cd ~/kafei-py && docker-compose up -d' kafei
